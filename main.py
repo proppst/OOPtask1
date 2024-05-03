692c6907d5b8b529b1819c8fad4e4f510acf1b32
Stanislav Propp <propp@mail.ru>
2024-05-03 15:04:36 +0500

    task4

--- main.py
+++ main.py
@@ -84,6 +84,23 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+def ever_student(students,name_course):
+    res=0
+    val=0
+    for st in students:
+        if name_course in st.courses_in_progress:
+            res+=st.ever_grade()
+            val+=1
+    return res/val
+
+def ever_lecture(lectures,name_course):
+    res=0
+    val=0
+    for le in lectures:
+        if name_course in le.courses_attached:
+            res+=le.ever_grade()
+            val+=1
+    return res/val
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
@@ -116,3 +133,38 @@ print(cool_Lecturer)
 print('Замена str student:')
 print(best_student)
 
+student1 = Student('Name1', 'Surname1', 'male')
+student1.courses_in_progress += ['Python']
+student2 = Student('Name2', 'Surname2', 'male')
+student2.courses_in_progress += ['SQL']
+
+cool_Reviewer.rate_hw(student1, 'Python', 7)
+cool_Reviewer.rate_hw(student1, 'Python', 5)
+cool_Reviewer.rate_hw(student2, 'SQL', 7)
+cool_Reviewer.rate_hw(student2, 'SQL', 8)
+
+cool_Lecturer2=Lecturer('Name2', 'Surname2')
+cool_Lecturer2.courses_attached += ['Python']
+
+cool_Lecturer3=Lecturer('Name3', 'Surname3')
+cool_Lecturer3.courses_attached += ['SQL']
+
+student1.rate_hw(cool_Lecturer2, 'Python', 9)
+student1.rate_hw(cool_Lecturer2, 'Python', 8)
+
+student2.rate_hw(cool_Lecturer3, 'SQL', 9)
+student2.rate_hw(cool_Lecturer3, 'SQL', 9)
+
+students_1=[]
+students_1.append(best_student)
+students_1.append(student1)
+students_1.append(student2)
+
+print(ever_student(students_1,'Python'))
+
+lectures_1=[]
+lectures_1.append(cool_Lecturer)
+lectures_1.append(cool_Lecturer2)
+lectures_1.append(cool_Lecturer3)
+
+print(ever_lecture(lectures_1,'Python'))
